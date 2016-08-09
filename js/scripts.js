07b00ec2dc6a275097281de13f877b185d4919c0
david bethune and francis prus <david@>
2016-08-09 11:25:21 -0700

    program finished

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
     $("body").removeClass();
     $("body").addClass("black-background");
   });
-});
-
-// $("button#white").click(function() {
-//   $("body").removeClass();
-//   $("body").addClass("white-background");
-//   });
 // });
+
+$("button#white").click(function() {
+  $("body").removeClass();
+
+ });
+});
